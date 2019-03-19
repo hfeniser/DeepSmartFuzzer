cb32f46ceecddb320fc476fe305a0e64689d8920
Samet Demir <demir.samet@hotmail.com>
2019-03-19 19:15:51 +0300

    sanity check inspected, coverage added & mutator hyperparameters updated

--- neuron_cov.py
+++ neuron_cov.py
@@ -6,7 +6,7 @@ from utils import get_layer_outs, percent_str
 from collections import defaultdict
 
 
-def measure_neuron_cov(model, test_inputs, scaler, threshold=0, skip_layers=None, outs=None):
+def measure_neuron_cov(model, test_inputs, scaler=None, threshold=0, skip_layers=None, outs=None):
     if outs is None:
         outs = get_layer_outs(model, test_inputs, skip_layers)
 
@@ -14,14 +14,15 @@ def measure_neuron_cov(model, test_inputs, scaler, threshold=0, skip_layers=None
 
     for layer_index, layer_out in enumerate(outs):  # layer_out is output of layer for all inputs
         for out_for_input in layer_out[0]:  # out_for_input is output of layer for single input
-            out_for_input = scaler(out_for_input)
-
+            #out_for_input = scaler(out_for_input)
+            
+            print("layer: ", layer_index, "shape: ", out_for_input.shape[-1])
             for neuron_index in range(out_for_input.shape[-1]):
                 activation_table[(layer_index, neuron_index)] = activation_table[(layer_index, neuron_index)] or\
                                                                 np.mean(out_for_input[..., neuron_index]) > threshold
 
     covered = len([1 for c in activation_table.values() if c])
     total = len(activation_table.keys())
-
+    print(total)
     return percent_str(covered, total), covered, total, outs
 
