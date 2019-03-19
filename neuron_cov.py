53434e7aa17e106f79a31d0ce1f4d8e9ff8d3a4d
Samet Demir <demir.samet@hotmail.com>
2019-03-19 19:18:52 +0300

    scale function added to neuron_cov.py

--- neuron_cov.py
+++ neuron_cov.py
@@ -6,7 +6,7 @@ from utils import get_layer_outs, percent_str
 from collections import defaultdict
 
 
-def measure_neuron_cov(model, test_inputs, scaler=None, threshold=0, skip_layers=None, outs=None):
+def measure_neuron_cov(model, test_inputs, scaler, threshold=0, skip_layers=None, outs=None):
     if outs is None:
         outs = get_layer_outs(model, test_inputs, skip_layers)
 
@@ -14,15 +14,19 @@ def measure_neuron_cov(model, test_inputs, scaler=None, threshold=0, skip_layers
 
     for layer_index, layer_out in enumerate(outs):  # layer_out is output of layer for all inputs
         for out_for_input in layer_out[0]:  # out_for_input is output of layer for single input
-            #out_for_input = scaler(out_for_input)
-            
-            print("layer: ", layer_index, "shape: ", out_for_input.shape[-1])
+            out_for_input = scaler(out_for_input)
+
             for neuron_index in range(out_for_input.shape[-1]):
                 activation_table[(layer_index, neuron_index)] = activation_table[(layer_index, neuron_index)] or\
                                                                 np.mean(out_for_input[..., neuron_index]) > threshold
 
     covered = len([1 for c in activation_table.values() if c])
     total = len(activation_table.keys())
-    print(total)
+
     return percent_str(covered, total), covered, total, outs
 
+def scale(intermediate_layer_output, rmax=1, rmin=0):
+    X_std = (intermediate_layer_output - intermediate_layer_output.min()) / (
+        intermediate_layer_output.max() - intermediate_layer_output.min())
+    X_scaled = X_std * (rmax - rmin) + rmin
+    return X_scaled
