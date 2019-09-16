7eb6d9c94f9e4ba9623452d8ca2f37ef9b088acb
Samet Demir <demir.samet@hotmail.com>
2019-09-16 17:05:18 +0300

    reorganize cifar

--- cifar_experiments/mcts_batch.py
+++ cifar_experiments/mcts_batch.py
@@ -1,9 +1,10 @@
 from mnist_lenet_experiment import mnist_lenet_experiment
 import numpy as np
+import itertools
 
-args, (train_images, train_labels), (test_images, test_labels), model, coverage, input_chooser = mnist_lenet_experiment("mcts")
+args, (train_images, train_labels), (test_images, test_labels), model, coverage, input_chooser = mnist_lenet_experiment("mcts_multi_image")
 
-np.random.seed(seed=213123)
+#np.random.seed(seed=191)
 
 test_input, _ = input_chooser()
 coverage.step(test_images)
@@ -14,7 +15,16 @@ from src.mcts import RLforDL_MCTS
 input_lower_limit = 0
 input_upper_limit = 255
 action_division_p1 = (1,3,3,1)
-actions_p2 = [-40, 40]
+
+translation = list(itertools.product(["translation"], [(10+10*k,10+10*k) for k in range(10)]))
+scale = list(itertools.product(["scale"], [(1.5+0.5*k,1.5+0.5*k) for k in range(10)]))
+shear = list(itertools.product(["shear"], [(-1.0+0.1*k,0) for k in range(10)]))
+rotation = list(itertools.product(["rotation"], [3+3*k for k in range(10)]))
+contrast = list(itertools.product(["contrast"], [1.2+0.2*k for k in range(10)]))
+brightness = list(itertools.product(["brightness"], [10+10*k for k in range(10)]))
+blur = list(itertools.product(["blur"], [k+1 for k in range(10)]))
+
+actions_p2 = translation + rotation + contrast + brightness + blur
 
 def tc1(level, test_input, best_input, best_coverage): 
     # limit the level/depth of root
@@ -30,10 +40,10 @@ def tc3(level, test_input, mutated_input):
     return  c1 or c2
 
 mcts = RLforDL_MCTS(test_input.shape, input_lower_limit, input_upper_limit,\
-     action_division_p1, actions_p2, tc1, tc2, tc3)
+     action_division_p1, actions_p2, tc1, tc2, tc3, with_implicit_reward=args.implicit_reward, verbose_image=True)
 
 for i in range(1, 1000):
-    test_input, test_label = input_chooser()
+    test_input, test_label = input_chooser(batch_size=64)
     root, best_input, best_coverage = mcts.run(test_input, coverage)
     if best_coverage > 0:
         input_chooser.append(best_input, test_label)
---
+++ cifar_experiments/mnist_lenet_experiment.py
@@ -0,0 +1,77 @@
+import parent_import # adds parent directory to sys.path
+
+import signal
+import sys
+def signal_handler(sig, frame):
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+def mnist_lenet_experiment(model_name, clustered_input_chooser=False):
+    import argparse
+
+    parser = argparse.ArgumentParser(description=str(model_name) + ' experiment for testing LeNet models with MNIST dataset')
+    parser.add_argument("--lenet", type=int, default=1, choices=[1,4,5])
+    parser.add_argument("--coverage", type=str, default="neuron", choices=["neuron","kmn","nbc","snac"])
+    parser.add_argument("--implicit_reward", type=bool, default=False)
+    args = parser.parse_args()
+
+    print("Arguments:", args)
+
+    import numpy as np
+
+    # MNIST DATASET
+    from keras.datasets import mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    train_images = train_images.reshape(-1,28,28,1).astype(np.int16)
+    test_images = test_images.reshape(-1,28,28,1).astype(np.int16)
+
+    # LENET MODEL
+    # multiple instance of openmp error 
+    # solution: set environment variable
+    # KMP_DUPLICATE_LIB_OK=TRUE
+    import os
+    os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+    from LeNet.lenet_models import LeNet1, LeNet4, LeNet5
+    from keras.layers import Input
+    if args.lenet == 1:
+        model = LeNet1(Input((28,28,1)))
+    elif args.lenet == 4:
+        model = LeNet4(Input((28,28,1)))
+    elif args.lenet == 5:
+        model = LeNet5(Input((28,28,1)))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # COVERAGE
+    if args.implicit_reward:
+        def calc_implicit_reward_neuron(p1, p2):
+            distance = np.abs(p1-p2)
+            implicit_reward =  1 / (distance + 1)
+            #print("p1, p2, distance, implicit_reward:", p1, p2, distance, implicit_reward)
+            return implicit_reward
+
+        def calc_implicit_reward(activation_values, covered_positions):
+            #print("activation_values, covered_positions", activation_values, covered_positions)
+            return np.max(activation_values * np.logical_not(covered_positions))
+    else:
+        calc_implicit_reward_neuron = None
+        calc_implicit_reward = None
+
+    if args.coverage == "neuron":
+        from coverages.neuron_cov import NeuronCoverage
+        coverage = NeuronCoverage(model, skip_layers=[0,5], calc_implicit_reward_neuron=calc_implicit_reward_neuron, calc_implicit_reward=calc_implicit_reward) # 0:input, 5:flatten
+    elif args.coverage == "kmn" or args.coverage == "nbc" or args.coverage == "snac":
+        from coverages.kmn import DeepGaugePercentCoverage
+        k = 20
+        coverage = DeepGaugePercentCoverage(model, k, train_images, skip_layers=[0,5], coverage_name=args.coverage, calc_implicit_reward_neuron=calc_implicit_reward_neuron, calc_implicit_reward=calc_implicit_reward) # 0:input, 5:flatten
+    else:
+        raise Exception("Unknown Coverage" + args.coverage)
+
+    if clustered_input_chooser:
+        from src.clustered_input_chooser import ClusteredInputChooser
+        input_chooser = ClusteredInputChooser(test_images, test_labels)
+    else:
+        from src.input_chooser import InputChooser
+        input_chooser = InputChooser(test_images, test_labels)
+
+    return args, (train_images, train_labels), (test_images, test_labels), model, coverage, input_chooser
